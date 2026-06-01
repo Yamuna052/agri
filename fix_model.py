daaadc15d14fd3ade23b4f7888490cddaf86b8b4
Yamuna052 <yamunakaligethi@gmail.com>
2026-06-01 21:44:24 +0530

    Delete fix_model.py

--- fix_model.py
+++
@@ -1,9 +0,0 @@
-from tensorflow.keras.models import load_model
-
-# load old model
-model = load_model("backend/models/soil_model.keras", compile=False)
-
-# re-save in new compatible format
-model.save("backend/models/soil_model_fixed.keras")
-
-print("Model re-saved successfully!")
